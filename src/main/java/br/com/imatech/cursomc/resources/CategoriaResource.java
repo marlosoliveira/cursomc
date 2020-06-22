3802e0c7d915658389ce1bfa83ab72caf238a6e5
Marlos Oliveira <marlos.iguatu@gmail.com>
2020-06-22 18:38:16 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/br/com/imatech/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/imatech/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package br.com.imatech.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.imatech.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String Listar() {
-		return "REST está funcionando";
+	public List<Categoria> Listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
