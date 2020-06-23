43989a7d0a616fc4774ac790cf97d63f8c411dc6
Marlos Oliveira <marlos.iguatu@gmail.com>
2020-06-22 22:04:59 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/br/com/imatech/cursomc/domain/Categoria.java
+++ src/main/java/br/com/imatech/cursomc/domain/Categoria.java
@@ -2,16 +2,24 @@ package br.com.imatech.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
 	public Categoria() {
 		
 	}
-
+	
 	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
