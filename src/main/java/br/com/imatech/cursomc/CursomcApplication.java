2f09046e5e8d420c56bbe244ec8df128ef5827aa
Marlos Oliveira <marlos.iguatu@gmail.com>
2020-06-23 18:39:13 -0300

    Criando operacao de instanciacao

--- src/main/java/br/com/imatech/cursomc/CursomcApplication.java
+++ src/main/java/br/com/imatech/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package br.com.imatech.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.apache.catalina.webresources.Cache;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import br.com.imatech.cursomc.domain.Categoria;
+import br.com.imatech.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "informática");
+		Categoria cat2 = new Categoria(null, "escritório");
+		repo.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
