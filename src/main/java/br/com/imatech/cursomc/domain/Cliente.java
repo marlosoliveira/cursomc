818862d096451760a576830f8756f727ff7ef427
Marlos Oliveira <marlos.iguatu@gmail.com>
2020-06-27 15:49:14 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/br/com/imatech/cursomc/domain/Cliente.java
+++ src/main/java/br/com/imatech/cursomc/domain/Cliente.java
@@ -14,6 +14,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import br.com.imatech.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -28,8 +30,10 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
+	
 	@ElementCollection
 	@CollectionTable(name="telefone")
 	private Set<String> telefones = new HashSet<>();
