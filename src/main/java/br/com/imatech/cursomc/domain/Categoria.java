fe468b3e73d5561386f06bfba678d92f6e5a8a5e
Marlos Oliveira <marlos.iguatu@gmail.com>
2020-06-28 17:50:23 -0300

    Atualizacao: utilizando somente jsonignore

--- src/main/java/br/com/imatech/cursomc/domain/Categoria.java
+++ src/main/java/br/com/imatech/cursomc/domain/Categoria.java
@@ -8,10 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
 import javax.persistence.ManyToMany;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 
 @Entity
@@ -23,7 +20,7 @@ public class Categoria implements Serializable{
 	private Integer id;
 	private String nome;
 	
-	@JsonManagedReference
+	
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
 	
